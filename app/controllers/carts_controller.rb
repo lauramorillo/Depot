24b0eafb7940d8c8e5b10867aaf4eaf891646425
Laura Morillo-Velarde Rodríguez <laura.morillovelarde@gmail.com>
2012-04-03 01:20:01 +0200

    Revert "Se permite vaciar la cesta por javascript."
    This reverts commit eae432256aba8e3ce485232439b5e8af87d1cd17.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -84,7 +84,6 @@ class CartsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to(store_url) }
       format.json { head :no_content }
-      format.js
     end
   end
 end
